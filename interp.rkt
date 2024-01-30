fe51d6b8b1d61b0a256303ccc4c0a5dab2aa65af
Max Harrison Jenkins <mahajenk@iu.edu>
2024-01-30 14:51:02 -0500

    Added tags for types and typechecker template

--- interp.rkt
+++ interp.rkt
@@ -1,17 +1,6 @@
 #lang racket
 (require rackunit)
 
-(define test-ct
-  (make-hash
-  `((Dog .  ;;key
-  (class-value (fields sound1 sound2) ((method (bark1 self)
-                                         (/ self sound1))
-                                 (method (bark2 self)
-                                         (/ self sound2))))))))
-(hash-set! test-ct 'Counter `(class-value (fields count) ((method (increment self n)
-                                 (+ (/ self count) n)))))
-
-
 (define lookup-ct
   (λ  (c c-def)
     (hash-ref c-def c)))
@@ -21,8 +10,10 @@
     (match `(,xs . (,args))
       ( `(() . ,_) env)
       ( `(,_ . ()) env)
-      ( `( (,x . ,xd) (,arg . ,ad))
+      ( `( (,x : ,t . ,xd) (,arg . ,ad))
         (extend-env* (extend-env env x arg c-def) xd ad c-def))
+      (`((,x : ,t . ,r) . (,arg))
+       (extend-env env x arg c-def))
       ( `((,x) . (,arg))
         (extend-env env x arg c-def)))))
 
@@ -76,6 +67,9 @@
       (`(+ ,e1 ,e2)
        (+ (value-of-exp e1 env c-def)
           (value-of-exp e2 env c-def)))
+      (`(- ,e1 ,e2)
+       (- (value-of-exp e1 env c-def)
+          (value-of-exp e2 env c-def)))
       (`(* ,e1 ,e2)
        (* (value-of-exp e1 env c-def)
           (value-of-exp e2 env c-def)))
@@ -84,7 +78,8 @@
       (`(zero? ,x)
        (zero? (value-of-exp x env c-def)))
       (`(new ,c . ,args)
-       `(object ,c . ,args))
+       (let ((val-args (value-of-list args env c-def)))
+       `(object ,c . ,val-args)))
       (`(send ,e ,g . ,es)
        (match-let ([ (and `(object ,c . ,args) o)  (value-of-exp e env c-def)]
                    [m-args (value-of-list es env c-def)]) 
@@ -94,10 +89,10 @@
                     (value-of-exp e env c-def)])
          (lookup-field c f args c-def)))
       (`,y #:when (symbol? y) (env y))
-      (`(λ ,x ,body)
+      (`(λ ,xs . (,body))
        (λ (arg)
          (value-of-exp body
-                       (extend-env* env x arg c-def)
+                       (extend-env* env xs arg c-def)
                        c-def)))
       (`(let ((,x ,v)) ,body)
        (let ((val (value-of-exp v env c-def)))
@@ -123,26 +118,20 @@
 
 
     ;basic interpreter tests
-(check-eqv? (value-of-exp '(+ 1 2) empty-env test-ct) 3)
-(check-eqv? (value-of-exp `((λ (x) x) 2) empty-env test-ct) 2)
-(check-eqv? (value-of-exp `((λ (x) (if (zero? x) #t #f)) 0) empty-env test-ct) #t)
-;field tests
-(check-eqv? (value-of-exp `(let ((c (new Counter 5))) (/ c count) ) empty-env test-ct) 5)
-;;method tests
-(check-eqv? (value-of-exp `(let ((c (new Counter 5))) (send c increment 10)) empty-env test-ct) 15)
-(check-eqv? (value-of-exp `(let ((boston-terrier (new Dog 1 22))) (send boston-terrier bark1)) empty-env test-ct) 1)
-(check-eqv? (value-of-exp `(let ((boston-terrier (new Dog 1 22))) (send boston-terrier bark2)) empty-env test-ct) 22)
+(check-eqv? (value-of-exp '(+ 1 2) empty-env '()) 3)
+(check-eqv? (value-of-exp `((λ (x : N) x) 2) empty-env '()) 2)
+(check-eqv? (value-of-exp `((λ (x : B) (if (zero? x) #t #f)) 0) empty-env '()) #t)
 ;;misc. tests
 (check-eqv? (value-of-exp `(if #t 1 2) empty-env (hash)) 1)
-(check-eqv? (value-of-exp `((λ (x y) (+ x y)) (2 3)) empty-env test-ct) 5)
-(check-eqv? (value-of-exp `((λ (a b c d) (+ b (+ a (+ c d))) ) (2 3 4 5)) empty-env test-ct) 14)
+(check-eqv? (value-of-exp `((λ (x : N y : N) (+ x y)) (2 3)) empty-env '()) 5)
+(check-eqv? (value-of-exp `((λ (a : N b : N c : N d : N) (+ b (+ a (+ c d))) ) (2 3 4 5)) empty-env '()) 14)
 
 (define test-prog-1
   `((class Dog
        (fields sound1 sound2)
-       ((method (bark1 self)
+       ((method (bark1 self : Dog)
                 (/ self sound1))
-        (method (bark2 self)
+        (method (bark2 self : Dog)
                 (/ self sound2))))
     (let ((boston-terrier (new Dog 1 22)))
       (send boston-terrier bark2))))
@@ -150,7 +139,7 @@
 (define test-prog-2
   `((class Rectangle
       (fields x y)
-      ((method (area self) ;;get area of the square
+      ((method (area self : Rectangle) ;;get area of the square
                (* (/ self x) (/ self y)))))
     
   (let ((myRectangle (new Rectangle 5 5)))
@@ -159,11 +148,22 @@
 (define test-prog-3
   `((class Adder
       (fields n)
-      ((method (add-to-n self x y)
+      ((method (add-to-n self : Adder x : N y : N)
                (+ (+ x y) (/ self n)))))
     (let ((myAdder (new Adder 7)))
       (send myAdder add-to-n 5 6))))
 
+(define test-prog-4
+  `((class Dog
+       (fields sound1 sound2)
+       ((method (bark1 self : Dog)
+                (/ self sound1))
+        (method (bark2 self : Dog)
+                (/ self sound2))))
+    (let ((boston-terrier (new Dog 1 (+ 10 12))))
+      (send boston-terrier bark2))))
+
 (check-eqv? (value-of-prog test-prog-1 (hash)) 22)
 (check-eqv? (value-of-prog test-prog-2 (hash)) 25)
 (check-eqv? (value-of-prog test-prog-3 (hash)) 18)
+(check-eqv? (value-of-prog test-prog-4 (hash)) 22)
---
+++ typechecker.rkt
@@ -0,0 +1,46 @@
+#lang racket
+(require rackunit)
+
+
+(define checker
+  (λ (e env ct)
+    (match e
+      (`,n #:when (number? n) (values e 'N))
+      (`,b #:when (boolean? b) (values e 'B))
+      (`(+ ,e1 ,e2) (let-values (((e1^ t1) (checker e1 env ct))
+                                 ((e2^ t2) (checker e2 env ct)))
+                      (check-type-eq? t1 'N e1)
+                      (check-type-eq? t2 'N e2) 
+                      (values e 'N)))  
+      (`(- ,e1 ,e2) e)
+      (`(* ,e1 ,e2) e)
+      (`(if ,e1 ,r1 ,r2) e)
+      (`(zero? ,x) e)
+      (`(new ,c . ,args) e)
+      (`(send ,e₁ ,g . ,es) e)            
+      (`(/ ,e₁ ,f) e)
+      (`,y #:when (symbol? y) (values e (env y)))
+      (`(λ ,x ,body) e)
+      (`(let ((,x ,v)) ,body) e)
+      (`(,rator ,rands) e))))
+
+
+(define check-type-eq?)
+(λ (t1 t2 e)
+  (cond
+    ((type-eq? t1 t2) #t)
+    (else (error 'type-check "~a != ~a\nin ~v" t1 t2 e))))
+
+(define type-eq?
+  (λ (t1 t2)
+    (match `(,t1 . ,t2)
+      (`(N . N) #t)
+      (`(B . B) #t)
+      (`(Self . Self) #t)
+      (`(,C . ,C) #:when (symbol? C) #t)
+      (`( (→ . ,ts1) . (→ . ,ts2)) (map type-eq? ts1 ts2))
+      (else #f))))
+
+
+(check-equal? (let-values (( (v t)  (checker `(+ 1 (+ 1 2)) '() '()))) t) 'N)
+(let-values (( (v t)  (checker `(+ #f (+ 1 2)) '() '()))) t)
