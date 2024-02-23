fdc6ee7347857953ecedf8b0a601b6404e093297
mjatkins <mjatkins3@outlook.com>
2024-02-23 16:21:56 -0500

    more tests + factorial class

--- interp.rkt
+++ interp.rkt
@@ -1,55 +1,50 @@
 #lang racket
 (require rackunit)
-(require "typechecker.rkt")
+(require "utils.rkt")
+;(require "typechecker.rkt")
 
 (define lookup-ct
   (λ  (c c-def)
     (hash-ref c-def c)))
 
 (define extend-env*
-  (λ (env xs args )
+  (λ (env xs args)
     (match `(,xs . (,args))
       ( `(() . ,_) env)
       ( `(,_ . ()) env)
-      ( `( (,x : ,t . ,xd) (,arg . ,ad))
+      ( `((,x : ,t . ,xd) (,arg . ,ad))
+        (extend-env* (extend-env env x arg) xd ad ))
+      ( `((,x  . ,xd) (,arg . ,ad))
         (extend-env* (extend-env env x arg) xd ad ))
       (`((,x : ,t . ,r) . (,arg))
        (extend-env env x arg))
       ( `((,x) . (,arg))
         (extend-env env x arg)))))
 
-
-                           
         
 (define lookup-field
   (lambda (c f args c-def)
-    (match-let (( `(class-value (fields . ,fs) ,_ ) (lookup-ct c c-def)))
+    (match-let (( `(class ,_ (fields . ,fs) ,_ ) (lookup-ct c c-def)))
       (get-arg args fs f))))
 
 (define get-arg
   (λ (args fs f)
     (cond
-      ((eqv? (car fs) f) (car args))
-      (else (get-arg (cdr args) (cdr fs) f)))))
-    
-(define lookup-method
-  (λ (c g c-def)
-    ;; class is defined as:
-    ;; (class-value (fields ...) ((method (g x ...) e ) ... ))
-    (match-let (( `(class-value ,_ . (,methods)) (lookup-ct c c-def)))
-      (findf (λ (m) (match-let (( `(method  (,g^ . ,_) ,body) m) )
-                      (eqv? g g^))) methods))))
+      ((eqv? (car fs) f)
+       (car args))
+      (else (get-arg (cdr args) (cdddr fs) f)))))
 
 (define value-of-list
   (λ (ls env c-def)
     (cond
       ((empty? ls) null)
       ((not (list? ls)) (value-of-exp ls env c-def))
-      (else (cons (value-of-exp (car ls) env c-def) (value-of-list (cdr ls) env c-def))))))
+      (else
+       (cons (value-of-exp (car ls) env c-def) (value-of-list (cdr ls) env c-def))))))
 
 (define apply-method
   (lambda (self m m-args c-def)
-    (match-letrec (( `(method (,_ . ,formals) ,body) m)
+    (match-let* (( `(method (,_ . ,formals) : ,_ ,body) m)
                    (env^ (extend-env*  empty-env formals `(,self . ,m-args))))
       (value-of-exp body env^ c-def))))    
 
@@ -92,9 +87,13 @@
          (lookup-field c f args c-def)))
       (`,y #:when (symbol? y) (env y))
       (`(λ ,xs . (,body)) (make-clos body xs env c-def))
-      (`(let ((,x ,v)) ,body)
-       (let ((val (value-of-exp v env c-def)))
-         (value-of-exp body (extend-env env x val) c-def)))
+      (`(let ,x-vs ,body)
+       (let ((xs '())
+             (vals '()))
+         (for ((x-v-ls x-vs))
+           (set! xs (cons (car x-v-ls) xs))
+           (set! vals (cons (value-of-exp (cadr x-v-ls) env c-def) vals)))
+         (value-of-exp body (extend-env* env xs vals) c-def)))
       (`(,rator . ,rands)
        (apply-clos
         (value-of-exp rator env c-def) (value-of-list rands env c-def))))))
@@ -111,15 +110,12 @@
   (λ (y) (error 'value-of-exp "unbound variable ~s" y)))
 
 
-
-
 (define value-of-prog
   (λ (p ct)
     (match p
       (`((class ,n . ,r ) . ,p^)
-       (value-of-prog p^ (hash-set ct n `(class-value . ,r))))
+       (value-of-prog p^ (hash-set ct n `(class ,n . ,r))))
       (`(,e) (value-of-exp e empty-env ct)))))
-
 
 
 
@@ -127,49 +123,22 @@
 (check-eqv? (value-of-exp '(+ 1 2) empty-env '()) 3)
 (check-eqv? (value-of-exp `((λ (x : N) x) 2) empty-env '()) 2)
 (check-eqv? (value-of-exp `((λ (x : B) (if (zero? x) #t #f)) 0) empty-env '()) #t)
+(check-eqv? (value-of-exp `(let ((x 6)
+                                  (y 7)
+                                  (z 12))
+                              (+ (+ x y) z)) empty-env (hash)) 25)
 ;;misc. tests
 (check-eqv? (value-of-exp `(if #t 1 2) empty-env (hash)) 1)
 (check-eqv? (value-of-exp `((λ (x : N y : N) (+ x y)) 2 3) empty-env '()) 5)
 (check-eqv? (value-of-exp `((λ (a : N b : N c : N d : N) (+ b (+ a (+ c d))) ) 2 3 4 5) empty-env '()) 14)
 
-(define test-prog-1
-  `((class Dog
-       (fields sound1 sound2)
-       ((method (bark1 self : Dog)
-                (/ self sound1))
-        (method (bark2 self : Dog)
-                (/ self sound2))))
-    (let ((boston-terrier (new Dog 1 22)))
-      (send boston-terrier bark2))))
 
-(define test-prog-2
-  `((class Rectangle
-      (fields x y)
-      ((method (area self : Rectangle) ;;get area of the square
-               (* (/ self x) (/ self y)))))
-    
-  (let ((myRectangle (new Rectangle 5 5)))
-    (send myRectangle area))))
-
-(define test-prog-3
-  `((class Adder
-      (fields n)
-      ((method (add-to-n self : Adder x : N y : N)
-               (+ (+ x y) (/ self n)))))
-    (let ((myAdder (new Adder 7)))
-      (send myAdder add-to-n 5 6))))
-
-(define test-prog-4
-  `((class Dog
-       (fields sound1 sound2)
-       ((method (bark1 self : Dog)
-                (/ self sound1))
-        (method (bark2 self : Dog)
-                (/ self sound2))))
-    (let ((boston-terrier (new Dog 1 (+ 10 12))))
-      (send boston-terrier bark2))))
 
 (check-eqv? (value-of-prog test-prog-1 (hash)) 22)
 (check-eqv? (value-of-prog test-prog-2 (hash)) 25)
 (check-eqv? (value-of-prog test-prog-3 (hash)) 18)
 (check-eqv? (value-of-prog test-prog-4 (hash)) 22)
+(check-eqv? (value-of-prog test-prog-5 (hash)) 13)
+(check-eqv? (value-of-prog test-prog-6 (hash)) 12)
+(check-eqv? (value-of-prog test-prog-8 (hash)) #t)
+(check-eqv? (value-of-prog test-prog-9 (hash)) 120)
--- typechecker.rkt
+++ typechecker.rkt
@@ -1,6 +1,19 @@
 #lang racket
+
 (require rackunit)
+(require "utils.rkt")
 (provide all-defined-out)
+(require racket/trace)
+
+
+(define prog-check
+  (λ (p ct)
+    (match p
+      (`(,(and `(class ,n . ,r) c) . ,p^)
+       (let ((ct^ (hash-set ct n c)))
+         (class-check n ct^)
+         (prog-check p^ ct^)))
+      (`(,e) (checker e empty-env ct)))))
 
 (define checker
   (λ (e env ct)
@@ -29,22 +42,89 @@
                                       (check-type-eq? tr1 tr2 e)
                                       (values e tr1))) ;;since both have the same type, just return tr1
       (`(zero? ,x) (let-values (( (x^ tx) (checker x env ct)))
-                     (check-type-eq? tx 'B x)
+                     (check-type-eq? tx 'N x)
                      (values e 'B)))
-      (`(new ,c . ,args) e)
-      (`(send ,e₁ ,g . ,es) e)            
-      (`(/ ,e₁ ,f) e)
-      (`(λ ,xs . (,body)) e)
+      (`(new ,c . ,args)
+       (let* ((arg-ts  (check-list args env ct))
+              (f-ts (lookup-field c ct)))
+         (if (not (equal? (length arg-ts) (length f-ts)))
+             (error 'checker "not enough arguments in: ~a~n" e)
+         (for ((f-t f-ts)
+               (arg-t arg-ts))
+           (check-type-eq? arg-t f-t e)))
+         (values e c)))
+      (`(send ,e₁ ,g . ,es)
+       (let*-values (((e₁^ te₁) (checker e₁ env ct))
+                    ((m-ts ) (m-type te₁ g ct))
+                    ((e-ts ) (check-list es env ct)))
+                    (for ((m-t m-ts)
+                          (e-t e-ts))
+                      (check-type-eq? m-t e-t e))
+                    (values e (get-method-return-type te₁ g ct))))            
+      (`(/ ,e₁ ,f)
+       (let*-values (((e₁^ te₁) (checker e₁ env ct))
+                                 ((c ) (lookup-ct te₁ ct))
+                                 ((fs ) (get-fs c))
+                                 ((t-f ) (get-f-type fs f)))
+                   (values e t-f)))
+      (`(λ ,xs . (,body)) (let*-values (((env^ e^) (values (extend-types-env xs env) e))
+                                          ((ts e^^) (values (get-arg-ts xs) e))
+                                          ((body^ tbody) (checker body env^ ct)))
+                            (values e (append `(→ ) ts `(,tbody)))))
       (`,y #:when (symbol? y) (values e (env y)))
-      (`(let ((,x ,v)) ,body) e)
-      (`(,rator ,rands) e))))
+      (`(let ((,x ,v) . ,r) ,body) ;;TODO: impement list for multiple let assiignments
+       (let*-values (( (x^ tx) (checker v env ct))
+                     ((env^ ) (extend-env env x tx))
+                     ((body^ tbody) (checker body env^ ct)))
+         (values e tbody)))
+      (`(,rator . ,rands)
+       (let-values (((rator^ rator-ts) (checker rator env ct))
+                    ((  rand-ts) (check-list rands env ct)))
+         (check-type-eq? (minus-last-elem rator-ts) (append '(→ ) rand-ts) e)
+         (values e rator-ts))))))
+
+(define m-type
+  (λ (cvar m ct)
+    (match-let ((`(method (,_ self : ,_ . ,vars) : ,_ ,_) (lookup-method cvar m ct)))
+      (get-arg-ts vars))))
+
+(define get-method-return-type
+  (λ (cvar m ct)
+    (match-let ((`(method (,_ self : ,_ . ,_) : ,rt ,_)  (lookup-method cvar m ct)))
+      rt)))
+
+
+
+(define class-check
+  (λ (c ct)
+    (match-let ((`(class ,cvar (fields . ,fs) ,methods) (lookup-ct c ct)))
+      (for ((m methods))
+        (method-check cvar m ct)))))
+
+(define method-check
+  (λ (cvar m ct)
+    (match-let ((`(method (,mvar . ,vars) : ,mt ,b ) m))
+      (if (or (eqv? (caddr vars) 'Self) (eqv? (caddr vars) cvar))
+          (checker b (extend-types-env vars empty-env) ct)
+          (error 'method-check "~s≠self~n" (caddr vars))))))
+      
+
+(define extend-types-env
+  (λ (xs env)
+    (match xs
+      (`(,x : ,tx) (extend-env env x tx))
+      (`(,x : ,tx . ,r) (extend-types-env r (extend-env env x tx))))))
 
 
 (define check-type-eq?
-(λ (t1 t2 e)
+  (λ (t1 t2 e)
   (cond
     ((type-eq? t1 t2) #t)
-    (else (error 'type-check "~a != ~a\nin ~v" t1 t2 e)))))
+    (else (error 'type-check "~a ≠ ~a\nin ~v" t1 t2 e)))))
+
+(define lookup-ct
+  (λ  (c c-def)
+    (hash-ref c-def c)))
 
 (define type-eq?
   (λ (t1 t2)
@@ -53,13 +133,135 @@
       (`(B . B) #t)
       (`(Self . Self) #t)
       (`(,C . ,C) #:when (symbol? C) #t)
-      (`( (→ . ,ts1) . (→ . ,ts2)) (map type-eq? ts1 ts2))
+      (`( (→ . ,ts1) . (→ . ,ts2)) (andmap type-eq? ts1 ts2))
       (else #f))))
 
+(define extend-env
+  (λ (env x arg) 
+    (λ (y)
+      (cond
+        ((eqv? y x) arg)
+        (else (env y))))))
 
-(check-equal? (let-values (( (v t)  (checker `(+ 1 (+ 1 2)) '() '()))) t) 'N)
+(define lookup-field
+  (lambda (c c-def)
+    (match-let (( `(class ,_ (fields . ,fs) ,_ ) (lookup-ct c c-def)))
+      (get-arg-ts fs))))
+
+(define get-arg
+  (λ (args fs f)
+    (cond
+      ((empty? args) (error 'get-arg "~s not found~n"f))
+      ((eqv? (car fs) f) (car args))
+      (else (get-arg (cdr args) (cdddr fs) f)))))
+
+(define get-f-type
+  (λ (fs f)
+    (match fs
+      ('() (error 'checker "field ~a not found~n" f))
+      (`(,f-name : ,f-type . ,r) #:when(eqv? f-name f) f-type)
+      (`(,f-name : ,f-type . ,r) (get-f-type r f)))))
+
+(define get-fs
+  (λ (c)
+    (match-let ((`(class ,_ (fields . ,fs) . ,_) c))
+      fs)))
+
+
+
+(define get-arg-ts
+  (λ (xs)
+    (match xs
+      ('() null)
+      (`(,x : ,tx) (cons tx '()))
+      (`(,x : ,tx . ,r) (cons tx (get-arg-ts r))))))
+
+(define check-list
+  (λ (ls env ct)
+    (cond
+      ((null? ls) null)
+      (else (let-values (( (e ts)  (checker (car ls) env ct)))
+              (cons ts (check-list (cdr ls) env ct)))))))
+
+(define minus-last-elem
+  (λ (ls) 
+    (cond
+      ((null? ls) null)
+      ((null? (cdr ls)) null)
+      (else (cons (car ls) (minus-last-elem (cdr ls)))))))
+(define empty-env
+  (λ (y) (error 'value-of-exp "unbound variable ~s" y)))
+
+
+
+(check-equal? (let-values (( (v t)  (checker `(+ 1 (+ 1 2)) empty-env '()))) t) 'N)
 (check-exn
  exn:fail?
- (λ () (let-values (( (v t)  (checker `(+ #f (+ 1 2)) '() '()))) t)))
-(check-equal? (let-values (( (v t)  (checker `(- 1 (+ 1 2)) '() '()))) t) 'N)
-(check-equal? (let-values (( (v t)  (checker `(- 1 (+ 1 (* 3 4))) '() '()))) t) 'N)
+ (λ () (let-values (( (v t)  (checker `(+ #f (+ 1 2)) empty-env '()))) t)))
+(check-equal? (let-values (( (v t)  (checker `(- 1 (+ 1 2)) empty-env '()))) t) 'N)
+(check-equal? (let-values (( (v t)  (checker `(- 1 (+ 1 (* 3 4))) empty-env '()))) t) 'N)
+(check-exn
+ exn:fail?
+ (λ ()
+   (checker `((λ (x : B) (if (zero? x) #t #f)) 2) empty-env '())))
+
+(check-equal? (let-values (( (v t) (checker `((λ (x : N) (if (zero? x) #t #f)) 2) empty-env '()))) t) `(→ N B))
+
+(check-exn
+ exn:fail?
+ (λ ()
+   (checker `((λ (x : B) (if (zero? x) #t #f)) #t) empty-env '())))
+
+(check-exn
+ exn:fail?
+ (λ ()
+   (checker `((λ (x : N) (if (5) #t #f)) 0) empty-env '())))
+(check-exn
+ exn:fail?
+ (λ ()
+   (checker `((λ (x : B) (if (zero? x) #t #f)) 2) empty-env '())))
+(check-exn
+ exn:fail?
+ (λ ()
+   (checker `((λ (x : B y : N z : N) x) 1 #t 4) empty-env '())))
+
+(check-equal? (let-values (( (v t) (checker `((λ (x : B y : N z : N) (if x (+ y z) 0)) #t 5 4) empty-env '()))) t) `(→ B N N N))
+
+(define class-address
+  `(class Adder
+      (fields n : N)
+      ((method (add-to-n self : Adder x : N y : N) : N
+               (+ (+ x y) (/ self n))))))
+
+
+(check-exn
+ exn:fail?
+ (λ ()
+   (checker `(let ((x #t)) (zero? x)) empty-env '())))
+(check-equal? (let-values (( (e te) (prog-check `(,class-address (new Adder 2))  (hash))))
+                te) 'Adder)
+(check-exn
+ exn:fail?
+ (λ ()
+   (prog-check `(,class-address (new Adder #f))  (hash))))
+
+(check-equal? (let-values (( (e te)
+                             (prog-check`(,class-address
+                                          (let ((myAdder (new Adder 7)))
+                                            (+ 6 (/ myAdder n)))) (hash)))) te)
+              'N)
+
+(check-equal?  (let-values (( (e t) (prog-check test-prog-1 (hash)))) t) 'N)
+(check-equal?  (let-values (( (e t) (prog-check test-prog-2 (hash)))) t) 'N)
+(check-equal?  (let-values (( (e t) (prog-check test-prog-3 (hash)))) t) 'N)
+(check-equal?  (let-values (( (e t) (prog-check test-prog-4 (hash)))) t) 'N)
+(check-equal?  (let-values (( (e t) (prog-check test-prog-5 (hash)))) t) 'N)
+(check-equal?  (let-values (( (e t) (prog-check test-prog-6 (hash)))) t) 'N)
+(check-exn
+ exn:fail?(λ () (prog-check test-prog-7 (hash))))
+(check-equal?  (let-values (( (e t) (prog-check test-prog-8 (hash)))) t) 'B)
+(check-equal?  (let-values (( (e t) (prog-check test-prog-9 (hash)))) t) 'N)
+(check-exn
+ exn:fail?(λ () (prog-check test-prog-10 (hash))))
+
+
