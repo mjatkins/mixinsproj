2a4def5099ad15b5862081c3f7a48cc693d69a69
mjatkins <mjatkins3@outlook.com>
2024-03-26 13:22:48 -0400

    Added mixins syntax for interpreter

--- interp.rkt
+++ interp.rkt
@@ -3,14 +3,13 @@
 (require "utils.rkt")
 (require racket/trace)
 
-(define lookup-ct
-  (λ  (c c-def)
-    (hash-ref c-def c)))
+
 
 (define lookup-field
-  (lambda (c f args c-def)
-    (match-let (( `(class ,_ (fields . ,fs) ,_ ) (lookup-ct c c-def)))
-      (get-arg args fs f))))
+  (lambda (c f args t)
+    (match-let (( `(class ,_ (fields . ,fs) (mix . ,ms) ,_ ) (get-class t c)))
+      (let (( all_fields (append fs (get-mixin-fields ms t)))) ;;preserve ordering from mix cluase for appending to fs
+        (get-arg args all_fields f)))))
 
 (define get-arg
   (λ (args fs f)
@@ -35,66 +34,70 @@
 
 
 (define value-of-exp
-  (λ (e env c-def)
+  (λ (e env t)
     (match e
       (`,n #:when (number? n) n)
       (`,b #:when (boolean? b) b)
       (`,s #:when (string? s) s)
       (`(+ ,e1 ,e2)
-       (+ (value-of-exp e1 env c-def)
-          (value-of-exp e2 env c-def)))
+       (+ (value-of-exp e1 env t)
+          (value-of-exp e2 env t)))
       (`(- ,e1 ,e2)
-       (- (value-of-exp e1 env c-def)
-          (value-of-exp e2 env c-def)))
+       (- (value-of-exp e1 env t)
+          (value-of-exp e2 env t)))
       (`(* ,e1 ,e2)
-       (* (value-of-exp e1 env c-def)
-          (value-of-exp e2 env c-def)))
+       (* (value-of-exp e1 env t)
+          (value-of-exp e2 env t)))
       (`(if ,e1 ,r1 ,r2)
-       (if (value-of-exp e1 env c-def) (value-of-exp r1 env c-def) (value-of-exp r2 env c-def)))
+       (if (value-of-exp e1 env t) (value-of-exp r1 env t) (value-of-exp r2 env t)))
       (`(zero? ,x)
-       (zero? (value-of-exp x env c-def)))
+       (zero? (value-of-exp x env t)))
       (`(new ,c . ,args)
-       (let ((val-args (value-of-list args env c-def)))
+       (let ((val-args (value-of-list args env t)))
        `(object ,c . ,val-args)))
       (`(send ,e ,g . ,es)
-       (match-let ([ (and `(object ,c . ,args) o)  (value-of-exp e env c-def)]
-                   [m-args (value-of-list es env c-def)]) 
-         (apply-method o (lookup-method c g c-def) m-args c-def)))
+       (match-let ([ (and `(object ,c . ,args) o)  (value-of-exp e env t)]
+                   [m-args (value-of-list es env t)])  
+         (apply-method o (lookup-method c g t) m-args t)))
       (`(/ ,e ,f) ;;in other notation, e.f
        (match-let ([`(object ,c . ,args)
-                    (value-of-exp e env c-def)])
-         (lookup-field c f args c-def)))
+                    (value-of-exp e env t)])
+         (lookup-field c f args t)))
       (`,y #:when (symbol? y) (env y))
-      (`(λ ,xs . (,body)) (make-clos body xs env c-def))
+      (`(λ ,xs . (,body)) (make-clos body xs env t))
       (`(let ,x-vs ,body)
        (let ((xs '())
              (vals '()))
          (for ((x-v-ls x-vs))
            (set! xs (cons (car x-v-ls) xs))
-           (set! vals (cons (value-of-exp (cadr x-v-ls) env c-def) vals)))
-         (value-of-exp body (extend-env* env xs vals) c-def)))
+           (set! vals (cons (value-of-exp (cadr x-v-ls) env t) vals)))
+         (value-of-exp body (extend-env* env xs vals) t)))
       (`(,rator . ,rands)
        (apply-clos
-        (value-of-exp rator env c-def) (value-of-list rands env c-def))))))
+        (value-of-exp rator env t) (value-of-list rands env t))))))
 
 (define make-clos
-  (λ (body xs env c-def)
-   `(make-clos ,body ,xs ,env ,c-def)))
+  (λ (body xs env t)
+   `(make-clos ,body ,xs ,env ,t)))
 
 (define apply-clos
   (λ (clos rands)
     (match clos
-      (`(make-clos ,body ,xs ,env ,c-def) (value-of-exp body (extend-env* env xs rands) c-def)))))
+      (`(make-clos ,body ,xs ,env ,t) (value-of-exp body (extend-env* env xs rands) t)))))
 (define empty-env
   (λ (y) (error 'value-of-exp "unbound variable ~s" y)))
 
 
 (define value-of-prog
-  (λ (p ct)
+  (λ (p t)
     (match p
-      (`((class ,n . ,r ) . ,p^)
-       (value-of-prog p^ (hash-set ct n `(class ,n . ,r))))
-      (`(,e) (value-of-exp e empty-env ct)))))
+      (`((mixin ,n . ,r ) . ,p^)
+        (add-mixin! t n `(mixin ,n . ,r))
+        (value-of-prog p^ t))
+       (`((class ,n . ,r ) . ,p^)
+        (add-class! t n `(class ,n . ,r))
+        (value-of-prog p^ t))
+       (`(,e) (value-of-exp e empty-env t)))))
 
 
 
@@ -105,23 +108,26 @@
 (check-eqv? (value-of-exp `(let ((x 6)
                                   (y 7)
                                   (z 12))
-                              (+ (+ x y) z)) empty-env (hash)) 25)
+                              (+ (+ x y) z)) empty-env (empty-global-table)) 25)
 ;;misc. tests
-(check-eqv? (value-of-exp `(if #t 1 2) empty-env (hash)) 1)
+(check-eqv? (value-of-exp `(if #t 1 2) empty-env (empty-global-table)) 1)
 (check-eqv? (value-of-exp `((λ (x : N y : N) (+ x y)) 2 3) empty-env '()) 5)
 (check-eqv? (value-of-exp `((λ (a : N b : N c : N d : N) (+ b (+ a (+ c d))) ) 2 3 4 5) empty-env '()) 14)
 
 
 
-(check-eqv? (value-of-prog test-prog-1 (hash)) 22)
-(check-eqv? (value-of-prog test-prog-2 (hash)) 25)
-(check-eqv? (value-of-prog test-prog-3 (hash)) 18)
-(check-eqv? (value-of-prog test-prog-4 (hash)) 22)
-(check-eqv? (value-of-prog test-prog-5 (hash)) 13)
-(check-eqv? (value-of-prog test-prog-6 (hash)) 12)
-(check-eqv? (value-of-prog test-prog-8 (hash)) #t)
-(check-eqv? (value-of-prog test-prog-9 (hash)) 120)
-(check-eqv? (value-of-prog test-prog-11 (hash)) #t)
-(check-eqv? (value-of-prog test-prog-12 (hash)) #f)
-(check-eqv? (value-of-prog test-prog-13 (hash)) 14)
-(check-eqv? (value-of-prog test-prog-14 (hash)) "foobar-town")
+(check-eqv? (value-of-prog test-prog-1 (empty-global-table)) 22)
+(check-eqv? (value-of-prog test-prog-2 (empty-global-table)) 25)
+(check-eqv? (value-of-prog test-prog-3 (empty-global-table)) 18)
+(check-eqv? (value-of-prog test-prog-4 (empty-global-table)) 22)
+(check-eqv? (value-of-prog test-prog-5 (empty-global-table)) 13)
+(check-eqv? (value-of-prog test-prog-6 (empty-global-table)) 12)
+(check-eqv? (value-of-prog test-prog-8 (empty-global-table)) #t)
+(check-eqv? (value-of-prog test-prog-9 (empty-global-table)) 120)
+(check-eqv? (value-of-prog test-prog-11 (empty-global-table)) #t)
+(check-eqv? (value-of-prog test-prog-12 (empty-global-table)) #f)
+(check-eqv? (value-of-prog test-prog-13 (empty-global-table)) 14)
+(check-eqv? (value-of-prog test-prog-14 (empty-global-table)) "foobar-town")
+(check-eqv?  (value-of-prog test-prog-15 (empty-global-table)) 1)
+(check-eqv?  (value-of-prog test-prog-16 (empty-global-table)) 3)
+
--- utils.rkt
+++ utils.rkt
@@ -1,17 +1,68 @@
 #lang racket
-(provide lookup-method lookup-ct class-address class-rectangle extend-env extend-env* test-prog-1 test-prog-2 test-prog-3 test-prog-4
-         test-prog-5 test-prog-6 test-prog-7 test-prog-8 test-prog-9 test-prog-10 test-prog-11 test-prog-12 test-prog-13 test-prog-14)
+(provide lookup-method class-address class-rectangle extend-env extend-env* test-prog-1 test-prog-2 test-prog-3 test-prog-4
+         test-prog-5 test-prog-6 test-prog-7 test-prog-8 test-prog-9 test-prog-10 test-prog-11 test-prog-12 test-prog-13 test-prog-14 test-prog-15 test-prog-16
+         get-class add-class! get-mixin add-mixin!
+         empty-global-table get-mixin-fields get-mixin-methods)
 
 
 ;;shared functions
 
+(struct global-table (classes mixins)#:transparent #:mutable)
+
+(define get-class
+  (λ  (table c-name)
+    (hash-ref (global-table-classes table) c-name)))
+
+(define add-class!
+  (λ (t c-name def)
+   (set-global-table-classes! t (hash-set (global-table-classes t) c-name def))))
+
+(define get-mixin
+  (λ  (table m-name)
+    (hash-ref (global-table-mixins table) m-name)))
+
+
+(define add-mixin!
+   (λ (t m-name def)
+     (set-global-table-mixins! t (hash-set (global-table-mixins t) m-name def))))
+
+(define empty-global-table
+  (λ ()
+    (global-table (hash) (hash))))
+
 (define lookup-method
-  (λ (c g c-def)
+  (λ (c g t)
     ;; class is defined as:
     ;; (class-value (fields ...) ((method (g x ...) e ) ... ))
-    (match-let (( `(class ,_ (fields . ,_) . (,methods)) (lookup-ct c c-def)))
-      (findf (λ (m) (match-let (( `(method  (,g^ . ,_) : ,_ ,body) m) )
-                      (eqv? g g^))) methods))))
+    (match-let (( `(class ,_ (fields . ,_) (mix . ,ms) . (,methods)) (get-class t c)))
+      (let ((all_methods  (append (get-mixin-methods ms t) methods))) ;;TODO: get methods from mixin list (ms-list is a list of the mixins)
+      (findf (λ (m) (match-let (( `(method  (,g^ . ,_) : ,_ ,body) m))
+                      (eqv? g g^))) all_methods)))))
+
+(define get-mixin-methods
+  (λ (ms-list table)
+    (match ms-list
+      (`() null)
+      (`(,m .,r) (append (get-mixin-method-body (get-mixin table m)) (get-mixin-methods r table))))))
+
+
+(define get-mixin-fields
+    (λ (ms-list table)
+    (match ms-list
+      (`() null)
+      (`(,m .,r) (append (get-mixin-field-list (get-mixin table m)) (get-mixin-fields r table))))))
+
+(define get-mixin-method-body
+  (λ (m-def)
+    (match m-def
+      (`(mixin ,m-name (fields . ,_) . (,method-decls)) method-decls))))
+
+
+(define get-mixin-field-list
+  (λ (m-def)
+    (match m-def
+      (`(mixin ,m-name (fields . ,field-list) . ,_) field-list))))
+
 (define extend-env
   (λ (env x arg) 
     (λ (y)
@@ -34,32 +85,39 @@
         (extend-env env x arg)))))
 
 
-(define lookup-ct
-  (λ  (c c-def)
-    (hash-ref c-def c)))
 
 ;;class-definitions
 
 (define class-address
   `(class Adder
-      (fields n : N)
+     (fields n : N)
+     (mix)
       ((method (add-to-n self : Adder x : N y : N) : N
                (+ (+ x y) (/ self n))))))
+
 (define broken-class
   `(class Broken
      (fields boolean : B number : N)
+     (mix)
      ((method (add-some-booleans self : Broken bool2 : B) : N
               (+ bool2 (/ self boolean))))))
 
+(define class-with-no-mix
+  `(class Broken
+     (fields n : N))) ;;the typechecker should reject this, as it fails to declare empty mixes.
+
+
 (define class-rectangle
   `(class Rectangle
-      (fields x : N y : N)
-      ((method (area self : Rectangle) : N ;;get area of the square
-               (* (/ self x) (/ self y))))))
+     (fields x : N y : N)
+     (mix)
+     ((method (area self : Rectangle) : N ;;get area of the square
+              (* (/ self x) (/ self y))))))
 
 (define class-car
   `(class Car
      (fields model-number : N new? : B miles-driven : N)
+     (mix)
      ((method (drive self : Car miles : N) : Car
               (new Car (/ self model-number) #f (+ (/ self miles-driven) miles)))
       (method (make-new-model self : Car new-model-number : N) : Car
@@ -68,6 +126,7 @@
 (define class-fact
   `(class Fact
      (fields n : N total : N)
+     (mix)
      ((method (factorial self : Fact) : N
               (if (zero? (/ self n))
                   (/ self total)
@@ -75,20 +134,23 @@
 (define even-class
   `(class Even
      (fields x : N)
+     (mix)
      ((method (even? self : Even) : B
               (if (zero? (/ self x))
                   #t
                   (send (new Odd (- (/ self x) 1)) even?))))))
 (define odd-class
-      `(class Odd
-         (fields x : N)
-         ((method (even? self : Odd) : B
-                  (if (zero? (/ self x))
-                      #f
-                      (send (new Even (- (/ self x) 1)) even?))))))
+  `(class Odd
+     (fields x : N)
+     (mix)
+     ((method (even? self : Odd) : B
+              (if (zero? (/ self x))
+                  #f
+                  (send (new Even (- (/ self x) 1)) even?))))))
 (define city-class
   `(class City
      (fields name : String population : N)
+     (mix)
      ((method (increasePopulation self : City new-residents : N) : City
       (new City (/ self name) (+ (/ self population) new-residents))))))
 
@@ -96,7 +158,8 @@
 
 (define test-prog-1
   `((class Dog
-       (fields sound1 : N sound2 : N)
+      (fields sound1 : N sound2 : N)
+      (mix)
        ((method (bark1 self : Dog) : N
                 (/ self sound1))
         (method (bark2 self : Dog) : N
@@ -107,6 +170,7 @@
 (define test-prog-2
   `((class Rectangle
       (fields x : N y : N)
+      (mix)
       ((method (area self : Rectangle) : N ;;get area of the square
                (* (/ self x) (/ self y)))))
 
@@ -120,7 +184,8 @@
 
 (define test-prog-4
   `((class Dog
-       (fields sound1 : N sound2 : N)
+      (fields sound1 : N sound2 : N)
+      (mix)
        ((method (bark1 self : Dog) : N
                 (/ self sound1))
         (method (bark2 self : Dog) : N
@@ -180,3 +245,65 @@
   `(,city-class
     (let ((myCity (new City "foobar-town" 7)))
       (/ myCity name))))
+
+
+(define test-prog-15
+  `((mixin incrementable
+        (fields n : N)
+      ((method (incr self : Self) : N
+       (+ (/ self n) 1))))
+    
+    (class Dog
+      (fields sound1 : N sound2 : N)
+      (mix incrementable)
+       ((method (bark1 self : Dog) : N
+                (/ self sound1))
+        (method (bark2 self : Dog) : N
+                (/ self sound2))))
+    (let ((boston-terrier (new Dog 1 22 0)))
+      (send boston-terrier incr))))
+
+(define test-prog-16 ;;testing multiple methods and fields in mixins
+  `((mixin incrementable
+        (fields n : N x : N y : N)
+      ((method (incr self : Self) : N
+               (+ (/ self n) 1))
+       (method (incr-by-2 self : Self) : N
+               (+ (/ self n) 2))
+       (method (incr-by-3 self : Self) : N
+               (+ (/ self n) 3))))
+    
+    (class Dog
+      (fields sound1 : N sound2 : N)
+      (mix incrementable)
+       ((method (bark1 self : Dog) : N
+                (/ self sound1))
+        (method (bark2 self : Dog) : N
+                (/ self sound2))))
+    (let ((boston-terrier (new Dog 1 22 0 1 2)))
+      (send boston-terrier incr-by-3))))
+
+
+
+
+;; errors from carbon tests:
+;; - circular mixing (mix only after creating such a mixin)
+;; - name errors (field not in mixin, naming clash)
+;; - mixin typing (an object cannot be an instanceof a mixin)
+;; - mixin only in class decl.
+;; - mixins mixing one field several times and self mixing
+
+
+;without the use of a super or inner keyword, each mixin should have a specific display method of its own that calls the previous display method.
+
+;to avoid naming conflicts as seen in the test cases from carbon, each method should have a unique name, otherwise a linear hierearchy need be setup via super or inner-like functionality.
+
+;in this case, a complete mixin/class that is mixing in a mixin M1 must contain every field from M1, thus making it complete.
+
+
+;;mixin decls. are defined as:
+;;(mixin m-name (fields f₁...) ((methods n self a₁) : aₙ₊₁ m-body))
+
+;;while class decls. are now:
+;;(class c-name (fields f₁...) (mix mix-names) ((methods n self a₁) : aₙ₊₁ m-body))
+
